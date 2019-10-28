f866facd04026b269147ae108a749eff27d4077a
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2019-10-28 18:36:06 +0800

    Remove sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './templates/navbar/navbar.component';
 import { RepoComponent } from './repo/repo.component';
 import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './templates/sidebar/sidebar.component';
 import { HelloOldDriverComponent } from './repo/hello-old-driver/hello-old-driver.component';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { HelloOldDriverComponent } from './repo/hello-old-driver/hello-old-drive
     NavbarComponent,
     RepoComponent,
     HomeComponent,
-    SidebarComponent,
     HelloOldDriverComponent
   ],
   imports: [

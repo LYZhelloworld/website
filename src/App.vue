a88f1115dfd9026bcc111012006d29fdf7e1e60c
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2021-02-22 11:15:24 +0800

    Add basic components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Home from "@/components/Home";
+import NavBar from "@/components/NavBar";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    Home
   }
 }
 </script>
 
 <style>
+body {
+  width: 100vw;
+  height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>

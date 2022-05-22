5e488e59397772de151961508a6a583d53d19063
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-05-22 22:24:07 +0800

    Recover files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar />
+  <HomePage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavBar,
+    HomePage
+}
 }
 </script>
 
 <style>
+body {
+  width: 100vw;
+  height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="dark">
+    <b-container>
+      <b-navbar-brand
+          class="text-primary"
+          href="https://github.com/LYZhelloworld/lyzhelloworld.github.io"
+          target="_blank"
+          rel="noopener noreferrer">
+        lyzhelloworld.github.io
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" active>Home</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-button
+              variant="outline-success"
+              href="https://github.com/LYZhelloworld"
+              target="_blank"
+              rel="noopener noreferrer">
+            Follow @LYZhelloworld
+          </b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
